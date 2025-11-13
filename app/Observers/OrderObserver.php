656cf850adaa4513c82efc148dbb951e72435367
vathana <siengvathana1@gmail.com>
2025-11-13 08:48:21 +0700

    upload project

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Order;
+use App\Events\OrderCancelled;
+use App\Events\TodayOrdersUpdated;
+use App\Models\Kot;
+use App\Events\OrderUpdated;
+use App\Events\OrderSuccessEvent;
+use App\Events\NewOrderCreated;
+
+
+class OrderObserver
+{
+
+    public function creating(Order $order)
+    {
+        if (branch() && $order->branch_id == null) {
+            $order->branch_id = branch()->id;
+        }
+    }
+
+    public function created(Order $order)
+    {
+        $todayKotCount = Kot::join('orders', 'kots.order_id', '=', 'orders.id')
+            ->whereDate('kots.created_at', '>=', now()->startOfDay()->toDateTimeString())
+            ->whereDate('kots.created_at', '<=', now()->endOfDay()->toDateTimeString())
+            ->where('orders.status', '<>', 'canceled')
+            ->where('orders.status', '<>', 'draft')
+            ->count();
+
+        event(new OrderUpdated($order, 'created'));
+        event(new TodayOrdersUpdated($todayKotCount));
+
+        // Dispatch event for new order notification
+        if ($order->status !== 'draft') {
+            event(new NewOrderCreated($order));
+        }
+    }
+
+    public function updated(Order $order)
+    {
+        if ($order->isDirty('status') && $order->status == 'canceled') {
+            OrderCancelled::dispatch($order);
+        }
+
+        $todayKotCount = Kot::join('orders', 'kots.order_id', '=', 'orders.id')
+            ->whereDate('kots.created_at', '>=', now()->startOfDay()->toDateTimeString())
+            ->whereDate('kots.created_at', '<=', now()->endOfDay()->toDateTimeString())
+            ->where('orders.status', '<>', 'canceled')
+            ->where('orders.status', '<>', 'draft')
+            ->count();
+
+        event(new OrderUpdated($order, 'updated'));
+        event(new TodayOrdersUpdated($todayKotCount));
+
+        event(new OrderSuccessEvent($order));
+    }
+}
