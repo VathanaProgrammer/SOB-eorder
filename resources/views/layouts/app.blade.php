40230e098cef54adf714667e8163e11ac90fef1f
vathana <siengvathana1@gmail.com>
2025-12-20 10:48:04 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -338,6 +338,6 @@
     <!-- Print Image Handler -->
     <script src="https://cdn.jsdelivr.net/npm/html-to-image@1.11.11/dist/html-to-image.min.js" data-navigate-track></script>
     <script src="{{ asset('js/print-image-handler.js') }}" data-navigate-track></script>
-    <script src="{{ asset('js/pos-offline.js') }}" data-navigate-track></script>
+    <script src="{{ asset('js/pos-offline.js') }}" ></script>
 </body>
 </html>
