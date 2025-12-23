de9df64c7e796189267012d43a3bd474951a4546
Seng Vengchhuong <leobomo5@gmail.com>
2025-12-23 10:46:11 +0700

    add hidden to veg

--- resources/views/livewire/shop/cart.blade.php
+++ resources/views/livewire/shop/cart.blade.php
@@ -367,7 +367,7 @@
             </div>
             <div class="flex flex-row flex-wrap items-center justify-end w-full col-span-2 gap-4 mt-2 md:col-span-1 sm:w-auto">
                 @if ($restaurant?->show_veg)
-                <label class="inline-flex items-center cursor-pointer" id="veg_toggle">
+                <label class="items-center cursor-pointer hidden" id="veg_toggle">
                     <input type="checkbox" value="1" wire:model.live='showVeg' class="sr-only peer">
                     <div
                         class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600">
